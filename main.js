44052ea0f31bbeeb762fc5b84126057fc47f3533
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-07-06 11:09:06 +0500

    alisher

---
+++ main.js
@@ -0,0 +1,7 @@
+var dollar = 9433.34;
+var euro = 10354.03;
+var expenses = ((500 * dollar) + (250 * dollar) + (120 * euro));
+var budget = prompt("enter your budget");
+
+if (budget >= expenses){alert("Oq yo’l, Alisher!" , document.writeln("Boradi kanewna"))}
+else {alert("Alisher, ozgina sabr qilish kerak bo’lar ekan." , document.writeln("Nma qladi soqqa ketqzp"))}
